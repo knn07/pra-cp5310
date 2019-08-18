23af36ebc0dc87c0a232caef57db580443fd134f
Khant Nyi Nyi <khantnyinyi46@gmail.com>
2019-08-18 08:39:02 +0800

    Week 5 practical is in main file.

---
+++ main/js/script.js
@@ -0,0 +1,71 @@
+function proalert() {
+    document.getElementById("ip").innerHTML="Product Announcement!"
+} 
+
+function product1(){
+    document.getElementById("prodes1").innerHTML="Made of a revolutionary polymer material layered for slim flexibility, Upper half of folded Galaxy Fold seen from the power and volume button side against a black and white type treatment that says Fold. The phone pulls back behind the type treatment and begins to unfold."
+    
+    document.getElementById("prod1price").innerHTML="$1,980"
+    document.getElementById("prod1name").innerHTML="Samsung Galaxy Fold"
+}
+
+function product2(){
+    
+    document.getElementById("prodes2").innerHTML="The Samsung Galaxy M20 is powered by the Exynos 7904 chipset. Built on a 14-nanometer process, the chipset uses a combination of two Cortex A73 cores clocked at 1.8GHz, paired with six Cortex A53 cores clocked at 1.6GHz."
+    
+    document.getElementById("prod2price").innerHTML="$220"
+    document.getElementById("prod2name").innerHTML="Samsung Galaxt M20"
+
+}
+
+function product3(){
+    
+    document.getElementById("prodes3").innerHTML="The phone comes with a 5.60-inch touchscreen display with a resolution of 1080x2220 pixels. Samsung Galaxy A8 (2018) is powered by a 1.6GHz octa-core processor. It comes with 4GB of RAM. The Samsung Galaxy A8 (2018) runs Android 7.1.1 and is powered by a 3,000mAh battery."
+    
+    document.getElementById("prod3price").innerHTML="$238"
+    document.getElementById("prod3name").innerHTML="Samsung Galaxt A8"
+}
+
+function product4(){
+    
+    document.getElementById("prodes4").innerHTML="It has a 5.8-inch Quad HD+ Super AMOLED display with an 18.5:9 aspect ratio. Touch response and colours are superb and it also supports HDR. In India, the phone uses an Exynos 9810 octa-core SoC and comes with 4GB of RAM a choice of 64GB or 256GB internal storage, which is expandable."
+    
+    document.getElementById("prod4price").innerHTML="$479"
+    document.getElementById("prod4name").innerHTML="Samsung Galaxt S9"
+}
+
+function product5(){
+    
+    document.getElementById("prodes5").innerHTML="Samsung Galaxy A7 (2018) runs on Android v8.0 (Oreo) operating system and driven by Octa-core (2.2 GHz, Dual core, Cortex A73 + 1.6 GHz, Hexa Core, Cortex A53) processor with 4GB of RAM. The smartphone features a compact body and 6.0(15.24 cm) display, having a screen resolution of 1080 x 2220 pixels."
+    
+    document.getElementById("prod5price").innerHTML="$290"
+    document.getElementById("prod5name").innerHTML="Samsung Galaxt A7"
+}
+
+function sc(){
+    
+function shoppingcart1(){
+    var name = document.getElementById("prod1name").value;
+    var price = document.getElementById("prod1price").value; 
+}
+
+function shoppingcart2(){
+    var name = document.getElementById("prod2name").value;
+    var price = document.getElementById("prod2price").value; 
+}
+
+function shoppingcart3(){
+    var name = document.getElementById("prod3name").value;
+    var price = document.getElementById("prod3price").value; 
+}
+
+function shoppingcart4(){
+    var name = document.getElementById("prod4name").value;
+    var price = document.getElementById("prod4price").value; 
+}
+
+function shoppingcart5(){
+    var name = document.getElementById("prod5name").value;
+    var price = document.getElementById("prod5price").value; 
+}
+}
